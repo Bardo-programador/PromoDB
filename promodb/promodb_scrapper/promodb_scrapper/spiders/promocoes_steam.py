df012203d8100855d241a5ece38fafb75046b4f7
samuel <developer.snowstorm590@passinbox.com>
2025-04-13 23:24:47 -0300

    Testes adicionados e spider da steam finalizada

---
+++ promodb/promodb_scrapper/promodb_scrapper/spiders/promocoes_steam.py
@@ -0,0 +1,43 @@
+import scrapy
+from promodb.promodb_scrapper.promodb_scrapper.items import PromodbScrapperItem
+
+from scrapy.selector import Selector
+class PromocoesSteamSpider(scrapy.Spider):
+    name = "promocoes_steam"
+    allowed_domains = ["store.steampowered.com"]
+    start_urls = [
+        "https://store.steampowered.com/search/?supportedlang=english&specials=1&hidef2p=1&ndl=1&page=1"
+    ]
+    pages = [start_urls[0]]
+    def parse(self, response):
+        # Pega os dados através de uma requisição json
+        total_pages = response.xpath('//*[@id="search_result_container"]/div[4]/div[2]/a[3]/text()').get()
+
+        for page in range(2, int(total_pages)+1):
+            self.pages.append(f"https://store.steampowered.com/search/?supportedlang=english&specials=1&hidef2p=1&ndl=1&page={page}")
+
+        for page in self.pages:
+            yield scrapy.Request(page, callback=self.parse_info_game)
+
+
+
+    def parse_info_game(self, response):
+        lista_jogos = self.extrai_lista_jogos(response)
+        item = PromodbScrapperItem()
+        for jogo in lista_jogos:
+            yield self.coleta_infos(jogo, item)
+
+
+    def extrai_lista_jogos(self, response):
+        return response.css("#search_resultsRows > a")
+
+    def coleta_infos(self, response, item):
+        item['link'] = response.xpath('@href').get()
+        item['nome'] = response.css('div.responsive_search_name_combined > div.col.search_name.ellipsis > span::text').get()
+        item['loja'] = 'Steam'
+        item['preco'] = float(response.css("div.responsive_search_name_combined > div.col.search_price_discount_combined.responsive_secondrow::attr(data-price-final)").get()) / 100
+        return item
+
+
+
+
---
+++ promodb/tests/steam_spider_test.py
@@ -0,0 +1,42 @@
+import pytest
+import scrapy
+from scrapy.http import HtmlResponse
+from twisted.internet import defer, reactor
+import aiohttp
+import sys
+import os
+
+# Adiciona a pasta correta ao sys.path
+
+from promodb.promodb_scrapper.promodb_scrapper.items import PromodbScrapperItem
+from promodb.promodb_scrapper.promodb_scrapper.spiders.promocoes_steam import PromocoesSteamSpider
+
+
+@pytest.mark.asyncio
+async def test_com_rede_real():
+    url = "https://store.steampowered.com/search/?supportedlang=english&specials=1&hidef2p=1&ndl=1&page=1"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            assert resp.status == 200
+            html = await resp.read()
+
+    fake_response = HtmlResponse(
+        url=url,
+        body=html,
+        encoding='utf-8'
+    )
+
+    spider = PromocoesSteamSpider()
+    lista = spider.extrai_lista_jogos(fake_response)
+
+    # Validação do conteúdo
+    assert len(lista) == 25, f"Esperado 25 links e retornou {len(lista)}"
+
+    item = PromodbScrapperItem()
+    for jogo in lista:
+        jogo = spider.coleta_infos(jogo, item)
+        assert jogo is not None
+        assert jogo["link"] is not None
+        assert jogo["nome"] is not None
+        assert jogo["preco"] is not None
