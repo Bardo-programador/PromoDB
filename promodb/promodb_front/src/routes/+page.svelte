f59e5f7185d9d044548be86fb20edba4f16a947f
samuel <developer.snowstorm590@passinbox.com>
2025-04-18 19:45:43 -0300

    Sveltekits adds

---
+++ promodb/promodb_front/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let data;
+  import Header from './Header.svelte'
+  let name = $state("Samuel")
+</script>
+<Header name={name}/>
+<h1>Promoções</h1>
+<ul>
+  {#each data.promocoes as promo}
+    <li>{promo.nome}: {promo.preco}</li>
+  {/each}
+</ul>
+<input type="text" bind:value={name}>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ promodb/promodb_front/src/routes/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let { name }:
+        {
+            name: string;
+        }= $props();
+</script>
+
+
+
+<h1>Meu Header: {name}</h1>
+
+
+<style>
+
+</style>
